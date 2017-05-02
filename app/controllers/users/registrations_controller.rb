5ea03921cfc76573a99da9953d1c8a4bbe3a243a
sokomheng <sokomheng89@gmail.com>
2017-05-02 12:10:03 +0700

    email is added for signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_name,
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :profile_name, :email,
                      :user_type, :username, :birth_date, :gender, :region_id, :province_id, :city])
   end
 
